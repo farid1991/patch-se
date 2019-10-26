83d2b16db4dc3af33f8ff6fbc6cf44fde7612400
Farid Irwan <farid_irwan@yahoo.com>
2019-10-27 03:22:36 +0700

    remove unused parts

--- include/MetaData.c
+++ include/MetaData.c
@@ -1,12 +1,14 @@
 #include "..\\include\Types.h"
 #include "..\\include\Function.h"
 
-#include "..\\include\interface\IMetaData.h"
+#include "..\\include\classes\IMetaData.h"
 #include "..\\include\types\IMetaData_types.h"
 
-#if defined(A2)
 void* MetaData_Desc_Create( wchar_t* fpath, wchar_t* fname )
 {
+  UUID IID_IMetaData={0xB1,0xEB,0x4F,0x97,0xB0,0xDD,0x42,0x09,0xB6,0x96,0xCE,0x11,0x2D,0x5E,0xD8,0xE8};
+  UUID CID_CMetaData={0x0B,0xCD,0x95,0x0F,0x9F,0xB6,0x4E,0x7A,0xA7,0xCD,0xCA,0x1D,0x87,0xBA,0x9A,0x27};
+  
   METADATA_DESC * MetaData_Desc = new METADATA_DESC;
   MetaData_Desc->pMetaData = NULL;
   MetaData_Desc->artist[0] = NULL;
@@ -16,21 +18,17 @@ void* MetaData_Desc_Create( wchar_t* fpath, wchar_t* fname )
   MetaData_Desc->genre[0] = NULL;
   MetaData_Desc->x6[0] = NULL;
   MetaData_Desc->x7[0] = NULL;
-  CoCreateInstance(CID_CMetaData, IID_IMetaData, PPINTERFACE(&MetaData_Desc->pMetaData));
+  CoCreateInstance(&CID_CMetaData, &IID_IMetaData, PPINTERFACE(&MetaData_Desc->pMetaData));
   MetaData_Desc->pMetaData->SetFile(fpath,fname);
   return(MetaData_Desc);
 }
-#endif
 
-#if defined(A2)
 void MetaData_Desc_Destroy( void* MetaData_Desc )
 {
   if (((METADATA_DESC*)MetaData_Desc)->pMetaData) ((METADATA_DESC*)MetaData_Desc)->pMetaData->Release();
   delete(MetaData_Desc);
 }
-#endif
 
-#if defined(A2)
 wchar_t* MetaData_Desc_GetTags( void* MetaData_Desc, int tagID )
 {
   METADATA_DESC * meta_desc = (METADATA_DESC*)MetaData_Desc;
@@ -62,18 +60,14 @@ wchar_t* MetaData_Desc_GetTags( void* MetaData_Desc, int tagID )
   meta_desc->pMetaData->GetTag(tagID,buf);
   return(buf);
 }
-#endif
 
-#if defined(A2)
 int MetaData_Desc_GetTrackNum( void* MetaData_Desc, int __NULL )
 {
   int track_num;
   ((METADATA_DESC*)MetaData_Desc)->pMetaData->GetTrackNum(__NULL,&track_num);
   return(track_num);
 }
-#endif
 
-#if defined(A2)
 int MetaData_Desc_GetCoverInfo( void* MetaData_Desc, char* cover_type, int* size, int* cover_offset )
 {
   COVER_INFO_DESC CoverInfo;
@@ -83,4 +77,3 @@ int MetaData_Desc_GetCoverInfo( void* MetaData_Desc, char* cover_type, int* size
   *cover_offset = CoverInfo.cover_offset;
   return TRUE;
 }
-#endif
