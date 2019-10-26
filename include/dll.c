5c6ea3e5f401eed3991d0b7be21e1b70cb92391a
Farid Irwan <farid_irwan@yahoo.com>
2019-10-26 15:16:59 +0700

    Add New Function
    int GetSignalQuality_dll( char * rssi, char * ber )

--- include/dll.c
+++ include/dll.c
@@ -141,3 +141,42 @@ int dll_GetImageWidth(wchar_t imageID)
   
   return image_width;
 }
+
+// GetSignalQuality ----------------------------------------------------
+
+struct GET_SIGNAL_QUALITY_SIGNAL
+{
+  SIGSELECT signo;
+  int null;
+  char unk_er;
+  char rssi;
+  char ber;
+  char error_cause;
+};
+
+union SIGNAL
+{
+  SIGSELECT signo;
+  struct GET_SIGNAL_QUALITY_SIGNAL hsig;
+};
+
+int GetSignalQuality_dll( char * rssi, char * ber )
+{
+  static const SIGSELECT sg[]={1,GetSignalQuality_Receive_SignalID};
+  char res=0;
+  PROCESS pid;
+  
+  hunt("LNH_ACC_SIDE/SL_Process",0,&pid,0);
+  union SIGNAL * mem = alloc(8,GetSignalQuality_Send_SignalID);
+  mem->hsig.null=0;
+  send(&mem,pid);
+  union SIGNAL * rec_buf = receive(sg);
+  if (rec_buf->signo==GetSignalQuality_Receive_SignalID)
+  {
+    rssi[0]=rec_buf->hsig.rssi;
+    ber[0]=rec_buf->hsig.ber;
+    res=rec_buf->hsig.unk_er;
+  }
+  free_buf(&rec_buf);
+  return(res);
+}
