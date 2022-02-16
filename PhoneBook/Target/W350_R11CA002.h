f529f470d3de9f646c25772a0f4b7bd346d1a873
farid1991 <farid.irwan.1991@gmail.com>
2022-02-17 05:20:09 +0700

    Update: +W350_R11CA002

---
+++ PhoneBook/Target/W350_R11CA002.h
@@ -0,0 +1,13 @@
+#ifndef W350_R11CA002
+#define W350_R11CA002
+
+#define A1
+#define PNX5230
+
+#define IMAGEID_PHONE 0xEB73
+#define IMAGEID_SIM 0xEB77
+
+#define TEXTID_PHONE 0xA48
+#define TEXTID_SIM 0xB76
+
+#endif
